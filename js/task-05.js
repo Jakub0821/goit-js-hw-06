fc81bfa96e9be11c1d5d3a06f2b0a301ea3c37f3
Jakub0821 <jakubmigda120@gmail.com>
2023-11-19 22:19:36 +0100

    push

---
+++ js/task-05.js
@@ -0,0 +1,11 @@
+const textInput = document.querySelector("#name-input");
+
+const textOutput = document.querySelector("#name-output");
+
+textInput.addEventListener("input", (event) => {
+ const inputValue = event.currentTarget.value;
+ if(inputValue === ""){
+    textOutput.textContent = "Anonymous";
+    } else {
+        textOutput.textContent = inputValue;}
+  });
